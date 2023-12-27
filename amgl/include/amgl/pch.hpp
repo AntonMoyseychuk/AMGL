1b932712977483ce8e9a3b239437845b48e40b4e
Anton Moyseychuk <moyseychuk2017@mail.ru>
2023-12-27 22:27:11 +0300

    feat: added amthreadpool and glm as private libs

---
+++ amgl/include/amgl/pch.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <vector>
+#include <array>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+
+#include <string>
+
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <atomic>
+#include <future>
+
+#include <functional>
